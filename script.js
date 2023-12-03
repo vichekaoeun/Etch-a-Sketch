7fd3bc8bb03b19b0936878bc931348bc709d3814
Vicheka Oeun <oeunvicheka95@gmail.com>
2023-12-03 16:41:24 -0500

    created loop to initialize grid

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 // Grid Setup
 const container = document.querySelector(".container");
 const title = document.createElement('div');
-const grid = document.createElement('div');
-const controls = document.createElement('div');
+const gridContainer = document.createElement('div');
 container.appendChild(title);
-container.appendChild(grid);
-container.appendChild(controls);
-grid.setAttribute("class", "grids");
+container.appendChild(gridContainer);
 title.setAttribute("class", "title");
+gridContainer.setAttribute("class", "gridContainer");
+let row = document.getElementsByClassName("row");
+let column = document.getElementsByClassName("column");
+
+for (i = 0; i < 16; i++) {
+    for (j = 0; j < 16; j++) {
+        const grid = document.createElement('div');
+        grid.setAttribute("class", "grids");
+        gridContainer.appendChild(grid);
+    }
+}
+
+
+const controls = document.createElement('div');
+container.appendChild(controls);
 controls.setAttribute("class", "controls");
 
 // Content setup
