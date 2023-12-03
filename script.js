e149e10f35a11c037988cc205ec02a440703cfac
Vicheka Oeun <oeunvicheka95@gmail.com>
2023-12-03 16:14:50 -0500

    skeleton setup & add attributes to DOM elements

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+// Grid Setup
 const container = document.querySelector(".container");
+const title = document.createElement('div');
 const grid = document.createElement('div');
+const controls = document.createElement('div');
+container.appendChild(title);
 container.appendChild(grid);
+container.appendChild(controls);
+grid.setAttribute("class", "grids");
+title.setAttribute("class", "title");
+controls.setAttribute("class", "controls");
+
+// Content setup
+const titlename = document.createElement('header');
+titlename.textContent = "Etch-A-Sketch";
+title.appendChild(titlename);
+
 
