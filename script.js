698627c0b0b21efaccc2433ea1dd3feeb608c524
Vicheka Oeun <oeunvicheka95@gmail.com>
2023-12-03 20:07:21 -0500

    made resize option & added button

--- script.js
+++ script.js
@@ -9,28 +9,45 @@ gridContainer.setAttribute("class", "gridContainer");
 let row = document.getElementsByClassName("row");
 let column = document.getElementsByClassName("column");
 
-// Loop to initialize Grid
-for (i = 0; i < 16; i++) {
-    const row = document.createElement('div');
-
-    for (j = 0; j < 16; j++) {
-        const grid = document.createElement('div');
-        grid.setAttribute("class", "grids");
-        gridContainer.appendChild(grid);
-
-        // Drawing Mechanic
-        grid.addEventListener('mouseover', function () {
-            this.classList.add("change-color");
-        });
-    }
-
-    gridContainer.appendChild(row);
-}
-
-
 const controls = document.createElement('div');
 container.appendChild(controls);
 controls.setAttribute("class", "controls");
+const sizebtn = document.createElement('button');
+
+controls.appendChild(sizebtn);
+sizebtn.setAttribute("class", "sizebtn");
+sizebtn.textContent = "Resize"
+
+row = 16;
+column = 16;
+
+sizebtn.addEventListener('click', () => {
+    row = prompt("Enter your desired row length");
+    column = prompt("Enter your desired column length");
+    createGrid(row, column);
+})
+createGrid(row, column)
+// Loop to initialize Grid
+function createGrid() {
+    for (i = 0; i < row; i++) {
+        const row = document.createElement('div');
+
+        for (j = 0; j < column; j++) {
+            const grid = document.createElement('div');
+            grid.setAttribute("class", "grids");
+            gridContainer.appendChild(grid);
+
+            // Drawing Mechanic
+            grid.addEventListener('mouseover', function () {
+                this.classList.add("change-color");
+            });
+        }
+
+        gridContainer.appendChild(row);
+    }
+}
+
+
 
 // Content setup
 const titlename = document.createElement('header');
